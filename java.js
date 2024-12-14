eec1f6bbba9602e3d083c1d05f8540c36b30ed54
Sol4nge <sol.ojeda2133@gmail.com>
2024-12-14 01:28:10 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -2,3 +2,14 @@ function toggle() {
     var trailer = document.querySelector(".trailer"); // Selecciona el contenedor del video
     trailer.classList.toggle("active"); // Activa o desactiva la visibilidad
 }
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src="img/sun.png"
+    } else{
+        icon.src="img/moon.png"
+    }
+}
